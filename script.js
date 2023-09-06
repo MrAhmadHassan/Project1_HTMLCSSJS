6f5c6b42fcc23679044d02c6b000276e10a5b625
Ahmad Hassan <ahmadhassan478626@gmail.com>
2023-09-06 21:35:34 +0500

    Completed module 1

---
+++ script.js
@@ -0,0 +1,15 @@
+// import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+
+function mousefollow(){
+    window.addEventListener("mousemove",function(dets) {
+        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px,${dets.clientY}px)`;
+    })
+}
+
+mousefollow()
